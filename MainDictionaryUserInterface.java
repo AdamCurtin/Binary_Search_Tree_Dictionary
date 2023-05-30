1125126017541d4247277bbb5df5f86659ee0386
Meghan Flaherty <131192974+flaherty-mi@users.noreply.github.com>
2023-05-29 18:01:05 -0700

    Add files via upload

--- MainDictionaryUserInterface.java
+++ MainDictionaryUserInterface.java
@@ -1,21 +1,17 @@
-/*
+// Meghan Flaherty & Adam Curtin
+// CS 145
+// Binary Search Tree Directory (lab 6)
+
+//package BinarySearchTree;
 
 
-Purpose: Purpose: Contains test cases to verify the functionality of the methods in the Manager class.
-Provides a menu-driven interface for users to interact with the dictionary.
-Handles user-input, displays options, and performs corresponding operations on the 
-dictionary class.
-*/
+public class MainDirectoryUserInterface {
 
-package binarySearchTreeDictionary;
+    public static void main(String[] args) {
+        
+        ManagerDictionary dictionary = new ManagerDictionary();
 
-public class MainDictionaryUserInterface {
+        dictionary.menu();
 
-	public static void main(String[] args) {
-		
-		ManagerDictionary dictionary = new ManagerDictionary();
-		
-		dictionary.menu();
-		
-	} // end main method
-} // end MainDictionaryUserInterface
+    }
+}
