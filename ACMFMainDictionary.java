a281b1bc54c342b6b135ed36f14ce66b413774e6
Adam Curtin <106624513+adamkurkey@users.noreply.github.com>
2023-05-30 23:41:18 -0700

    Add files via upload
    Added sources for sites I researched traversals on.

--- ACMFMainDictionary.java
+++ ACMFMainDictionary.java
@@ -3,6 +3,12 @@
 //Binary Search Tree Directory (lab 6)
 //Purpose: Serves as the entry point of the program. 
 //Creates instance of Manager Class and calls its methods to display user interface.
+/*
+ * Source citation:
+ * https://www.geeksforgeeks.org/implementing-inorder-preorder-postorder-using-stack-in-java/#
+ * https://www.enjoyalgorithms.com/blog/iterative-binary-tree-traversals-using-stack
+ * https://www.educative.io/answers/how-to-perform-an-iterative-inorder-traversal-of-a-binary-tree
+ */
 
 package adamMeghanBinarySearchTree;
 
